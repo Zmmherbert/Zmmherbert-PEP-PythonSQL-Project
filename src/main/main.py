9321eda322d5b97ca20d6d060f742e3d13ab0122
Zmmherbert <Zmmherbert@github.com>
2026-07-24 22:47:49 +0000

    Test run: 3/4 passed, 1 failed, 0 skipped - 2026-07-24T22:47:49.907Z

--- src/main/main.py
+++ src/main/main.py
@@ -55,6 +55,7 @@ def load_and_clean_users(file_path):
             if len(row) == len(header) and all(entry.strip() != '' for entry in row):
                 print(row)
                 cursor.execute(f'INSERT INTO users ({(',').join(header)}) VALUES({(','.join(['?'] * len(header)))})', row)
+    print(cursor.execute('SELECT * FROM users').fetchall())
 
 
 # This function will load the callLogs.csv file into the callLogs table, discarding any records with incomplete data
@@ -76,6 +77,8 @@ def write_user_analytics(csv_file_path):
         writer.writerow(['userId', 'avgDuration', 'numCalls'])
 
         num_users = cursor.execute('SELECT COUNT(*) FROM users').fetchone()[0]
+        print(cursor.execute('SELECT * FROM users').fetchall())
+        print(num_users)
         
         for i in range(num_users):
             avg_duration = cursor.execute(f'SELECT AVG(endTime - startTime) FROM callLogs WHERE userId = {i+1}').fetchone()[0]
